eee882d4f909287c51a42c77d9e9d9bfae945042
anandajuliar <anandajulia001@gmail.com>
2024-03-14 17:49:48 +0700

    menambah input

--- 046_Insertionsort/046_Insertionsort.cpp
+++ 046_Insertionsort/046_Insertionsort.cpp
@@ -4,6 +4,27 @@ using namespace std;
 int arr[9];
 int n;
 
+void input() { // procedur untuk input
+    while (true) {
+        cout << "masukkan banyaknya elemen array :"; // Output ke layar
+        cin >> n; // Input dari pengguna
+        if (n <= 20) // Jika n kurang dari atau sama dengan 20
+            break; // Keluar dari loop
+        else { // Jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n"; // Output ke layar
+        }
+    }
+    cout << endl; // Output baris kosong
+    cout << "=====================" << endl; // Output ke layar
+    cout << "Masukkan elemen array" << endl; // Output ke layar
+    cout << "=====================" << endl; // Output ke layar
+
+    for (int i = 0; i < n; i++) { // Looping dengan i dimulai dari 0 hingga n
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> a[i]; // Input dari pengguna
+    }
+}
+
 void insertionSort() {  //prosedur
     int i, temp, j;
 
@@ -21,6 +42,7 @@ void insertionSort() {  //prosedur
         arr[j + 1] = temp;      //step 5
     }
 }
+
 
 int main()
 {
